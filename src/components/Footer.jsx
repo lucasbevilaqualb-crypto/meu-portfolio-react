d518d97b7327358fe25f1031123eade9708bdba1
lucasbevilaqualb-crypto <lucasbevilaqua.lb@gmail.com>
2026-04-17 23:39:24 -0300

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="rodape-pagina">
+      <p>&copy; 2026 — Criado por Lucas Rodrigues Bevilaqua</p>
+    </footer>
+  );
+}
+
+export default Footer;
